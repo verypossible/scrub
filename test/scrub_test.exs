7d0fb3cd08f89ed4ea491bf6116943d92d62ebe3
maguirre <melvin@verypossible.com>
2020-05-04 11:51:34 -0500

    Add Initial Tests suite
    Why:
    In order to provide a test that confirm a base functionality
    
    How:
    Adding tests that confirm Open connection, tag retreival and close connection functionality

--- test/scrub_test.exs
+++ test/scrub_test.exs
@@ -2,27 +2,31 @@ defmodule ScrubTest do
   use ExUnit.Case
   alias Scrub.CIP.Connection
 
-  doctest Scrub
-  use ExUnit.Case, async: false
-  setup_all do
-    {:ok, session} =
-      Scrub.open_session("20.0.0.70")
-      :ok = wait_for_session(session)
-    %{session: session}
-  end
 
-  test "can open a connection", %{session: session} do
+  doctest Scrub
+  @plc_address "20.0.0.70"
+
+
+  @tag :with_plc
+  test "can open a connection" do
+    {:ok, session} =
+      Scrub.open_session(@plc_address)
+    :ok = wait_for_session(session)
     assert {_, %Connection{ }} = Scrub.open_conn(session)
   end
 
-  test "can receive retrieve a tag", %{session: session} do
+  @tag :with_plc
+  test "can receive retrieve a tag" do
+    {:ok, session} =
+      Scrub.open_session(@plc_address)
+    :ok = wait_for_session(session)
     assert {:ok, _} = Scrub.read_tag(session,"P1_HMI_INK_DINT_ARRAY3")
   end
 
-
-  test "can close a session", %{session: _} do
+  @tag :with_plc
+  test "can close a session" do
     {:ok, session} =
-      Scrub.open_session("20.0.0.70")
+      Scrub.open_session(@plc_address)
       :ok = wait_for_session(session)
     assert {:ok, _ } = Scrub.close_session(session)
 
